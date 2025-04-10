7633ac7f8b437187f9e20e7d0d3ff08b22a03bc7
Merid Berhe <31586679+merid-berhe@users.noreply.github.com>
2025-04-10 14:16:36 +0200

    Initial Commit

---
+++ OiOi/OiOiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OiOiApp.swift
+//  OiOi
+//
+//  Created by Merid Berhe on 10.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct OiOiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OiOiTests/OiOiTests.swift
@@ -0,0 +1,17 @@
+//
+//  OiOiTests.swift
+//  OiOiTests
+//
+//  Created by Merid Berhe on 10.04.2025.
+//
+
+import Testing
+@testable import OiOi
+
+struct OiOiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
